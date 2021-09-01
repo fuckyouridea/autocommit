452c16a81c908b299c0182affddeea823a6ba4ba
mrclfd <mrclfd@yahoo.com>
2021-09-01 19:38:51 +0700

    task: improvements

--- task.sh
+++ task.sh
@@ -1,31 +1,35 @@
 #!/bin/bash
 
+# set timezone to Asia/Jakarta WIB
+sudo ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+
+# random title for commit
 inpo[0]="bot: 👋 🆎🆖"
 inpo[1]="bot: 😒 Y"
 inpo[2]="bot: 😣 Kesel John, ndak kuat Aku"
 inpo[3]="bot: 😁 Kerjo, kerjo, kerjo!"
 inpo[4]="bot: 😪 Hadeehh..."
 inpo[5]="bot: 😀 Aku siap, Aku siap"
-inpo[6]="bot: 😤 Kapan rabi Aku Cok! yn mbendino ngommit wae, babi og"
+inpo[6]="bot: 🙃 YNTKTS"
 inpo[7]="bot: 😴 zZz"
-inpo[8]="bot: 🙄 Wis ora digaji, dikongkon terus_-"
+inpo[8]="bot: 😶 ndak og"
 inpo[9]="bot: 😬 Sek Ndes, rep ngising"
 inpo[10]="bot: 😪 Pegel tenan"
 inpo[11]="bot: 😋 Mbak Anya, Aku padamuuu"
 inpo[12]="bot: 😂 Joss"
 inpo[13]="bot: 😪 Hari-hari commit :("
-inpo[14]="bot: 🙃 YNTKTS"
-inpo[15]="bot: 😶 ndak og"
 
 rand=$[$RANDOM % ${#inpo[@]}]
 
-# Set timezone to Asia/Jakarta WIB
-sudo ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+# time formatting
+d=`date '⌚ %H:%M:%S WIB 📅 %d/%m/%Y'`
 
-d=`date '+%Y-%m-%dT%H:%M:%SZ'`
+# update.md content
+echo "---
+### 😬 LAST UPDATED AT: `${d}`
+---" > update.md
 
-echo "### 😬 LAST UPDATED AT: ${d}" > update.md
-
-git config --local user.email "mrclfd@yahoo.com"
-git config --local user.name "mrclfd"
-git commit -am "${inpo[$rand]} (at ${d})"
+# run command
+git config --local user.email "mrclfd@yahoo.com" # change to your github account email
+git config --local user.name "mrclfd" # change to your github account username
+git commit -am "${inpo[$rand]} `${d}`"
