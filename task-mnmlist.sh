9b5a4b24e32799a4aa2ad64cd977195046865fb2
mrclfd <mrclfd@yahoo.com>
2021-09-02 18:54:49 +0700

    improvements

--- task-mnmlist.sh
+++ task-mnmlist.sh
@@ -3,34 +3,34 @@
 ## set timezone to Asia/Jakarta WIB
 sudo ln -sf /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
-##ori random title for commit
-# inpo[0]="bot: 👋 🆎🆖"
-# inpo[1]="bot: 😒 Y"
-# inpo[2]="bot: 😣 Kesel John, ndak kuat Aku"
-# inpo[3]="bot: 😁 Kerjo, kerjo, kerjo!"
-# inpo[4]="bot: 😪 Hadeehh..."
-# inpo[5]="bot: 😀 Aku siap, Aku siap"
-# inpo[6]="bot: 🙃 YNTKTS"
-# inpo[7]="bot: 😴 zZz"
-# inpo[8]="bot: 😶 ndak og"
-# inpo[9]="bot: 😬 Sek Ndes, rep ngising"
-# inpo[10]="bot: 😪 Pegel tenan"
-# inpo[11]="bot: 😋 Mbak Anya, Aku padamuuu"
-# inpo[12]="bot: 😂 Joss"
-# inpo[13]="bot: 😪 Hari-hari commit :("
-# inpo[13]="bot: 😎 GG Gaming Gak Guys?!"
+## random emoji for commit
+emoji[0]="✨"
+emoji[1]="😎"
+emoji[2]="❤"
+emoji[3]="👀"
+emoji[4]="🤡"
+emoji[5]="👻"
+emoji[6]="🐐"
+emoji[7]="🐵"
+emoji[8]="🎅"
+emoji[9]="🧛‍"
+emoji[10]="👍"
+emoji[11]="🎉"
+emoji[12]="♠"
+emoji[13]="🗿"
+emoji[13]="🍃"
 
-#ori rand=$[$RANDOM % ${#inpo[@]}]
+acak=$[$RANDOM % ${#emoji[@]}]
 
 ## time formatting
 jam=`date '+%H:%M:%S'`
 tanggal=`date '+%d/%m/%Y'`
 
 ## update.md content
-echo "<h2 align="center">😬 TERAKHIR DIPERBARUI: ⌚ <code>${jam} WIB</code> 📅 <code>${tanggal}</code></h2>" > update.md
+echo "<h2 align="center">✨ LAST UPDATED: ⌚ <code>${jam} WIB</code> 📅 <code>${tanggal}</code></h2>" > update.md
 
 ## execute command
 git config --local user.email "mrclfd@yahoo.com" # change to your github account email
 git config --local user.name "mrclfd" # change to your github account username
 #ori git commit -am "${inpo[$rand]} \`⌚ ${jam} WIB 📅 ${tanggal}\`"
-git commit -am "bot: ⌚ \`${jam} WIB\` 📅 \`${tanggal}\`"
+git commit -am "bot: ${emoji[$acak]} \`⌚ ${jam} WIB 📅 ${tanggal}\`"
